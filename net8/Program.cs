38130d19e2f4d20b2435ade7bdf833d12e806fb1
Julie Clarebots <julie.clarebots@gmail.com>
2024-03-27 11:56:06 +0100

    Initial Commit

---
+++ net8/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace net8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var fs = System.IO.File.Open("testmessage.msg", FileMode.Open))
+            {
+                using (var b = new BinaryReader(fs))
+                {
+                    byte[] blob = b.ReadBytes((int)fs.Length);
+                    using (var msg = new MsgReader.Outlook.Storage.Message(new MemoryStream(blob)))
+                    {
+                        Console.WriteLine(msg.Subject);
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
